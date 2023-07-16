7e0779cf65088848fb02e05efa761f55a9d431d0
myli5 <41824084+myli5@users.noreply.github.com>
2023-07-16 16:59:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner, Nathan is going for excercise, Kids are awesome!')
+streamlit.title('My Parents New Healthy Diner, Nathan is going for excercise, Kids are awesome! Thillai graduation party!')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3')
