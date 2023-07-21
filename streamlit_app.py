46d47af5077dcd02fdefbc608439ae70d42e848b
myli5 <41824084+myli5@users.noreply.github.com>
2023-07-20 22:11:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-'''
+
 
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -76,3 +76,4 @@ fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfru
 streamlit.write('Thanks for adding ', fruit_choice)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
+'''
